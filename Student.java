13a3545ac933a0bfe455e000dbd9fbd91d072f27
Slobodan Marinkovik <slobodan@echoios.com>
2020-04-12 20:23:04 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -5,14 +5,56 @@ class Student {
 	Int[] labPoints;
 
 	//TODO constructor
+    public Student(String i, String f, Student l, Int[] labP) {
+        index = i;
+        firstName = f;
+        lastName = l;
+        labPoints = labP;
+    }
 
 	//TODO seters & getters
+    public void setName(String newName) {
+        this.firstName = newName;
+    }
+    
+    public void setLastName(String newLastName) {
+        this.lastName = newLastName;
+    }
+    
+    public void setIndex(String newIndex) {
+        this.index = newIndex;
+    }
+    
+    public void setLabPoints(Int[] newLabPoints) {
+        this.labPoints = newLabPoints;
+    }
+    
+    public String getIndex() {
+        return index;
+    }
+    
+    public String getName() {
+        return firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Int[] getLabPoints() {
+        return labPoints;
+    }
+    
 	public double getAverage() {
-		//TODO
+        double total = 0;
+        for(int i=0; i<labPoints.length; i++){
+            total = total + arr[i];
+        }
+        double average = total / arr.length;
+        return average
 	}
 
 	public boolean hasSignature() {
-		//TODO
+        return labPoints.size()>8;
 	}
 }
